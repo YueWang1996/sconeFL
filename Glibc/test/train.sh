da2ac8f8f3703605ab68deb03ec074885ae85ba3
YueWang1996 <YueWang1996@users.noreply.github.com>
2021-05-26 21:25:39 +0200

    Update train.sh

--- Glibc/test/train.sh
+++ Glibc/test/train.sh
@@ -2,10 +2,9 @@
 
 export PATH_TO_REDIS_BIN=/usr/local/bin/ && export REDIS_PORT=6379 && export LOCALHOST=localhost && export PYTHON=/opt/_internal/cpython-3.6.0/bin/python3
 
-cd /PaddleFL/python/paddle_fl/mpc/examples/model_encryption/train
-python3 ../process_data.py
-cd ../train
-cp -r mpc_data/ ../
-cp /PaddleFL/python/paddle_fl/mpc/examples/run_standalone.sh .
-bash run_standalone.sh encrypt_and_train_model.py
-python3 decrypt_mpc_loss.py
+cd /PaddleFL/python/paddle_fl/mpc/examples/model_encryption/train \
+&& python3 ../process_data.py \
+&& cd ../train \
+&& cp -r mpc_data/ ../ \
+&& bash ../run_standalone.sh encrypt_and_train_model.py \
+&& python3 decrypt_mpc_loss.py
