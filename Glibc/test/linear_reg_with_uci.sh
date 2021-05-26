29a9938b482fb850c7607105bc1b0e892cb61d20
YueWang1996 <YueWang1996@users.noreply.github.com>
2021-05-26 12:50:59 +0200

    Update linear_reg_with_uci.sh

--- Glibc/test/linear_reg_with_uci.sh
+++ Glibc/test/linear_reg_with_uci.sh
@@ -1,5 +1,7 @@
 # commands used for https://github.com/PaddlePaddle/PaddleFL/blob/master/python/paddle_fl/mpc/examples/linear_reg_with_uci/README.md
 
+# export PATH_TO_REDIS_BIN=/usr/local/bin/ && export REDIS_PORT=6379 && export LOCALHOST=localhost && export PYTHON=/opt/_internal/cpython-3.6.0/bin/python3
+
 cd /PaddleFL/python/paddle_fl/mpc/examples/linear_reg_with_uci
 python3 prepare.py
 cp /PaddleFL/python/paddle_fl/mpc/examples/run_standalone.sh .
