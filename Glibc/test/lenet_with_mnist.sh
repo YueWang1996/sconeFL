b14402a4f5a58951123c673d166231d945a02c9c
YueWang1996 <YueWang1996@users.noreply.github.com>
2021-05-26 20:42:20 +0200

    Create lenet_with_mnist.sh

---
+++ Glibc/test/lenet_with_mnist.sh
@@ -0,0 +1,12 @@
+# commands for https://github.com/PaddlePaddle/PaddleFL/blob/master/python/paddle_fl/mpc/examples/lenet_with_mnist/README_CN.md
+
+export PATH_TO_REDIS_BIN=/usr/local/bin/ && export REDIS_PORT=6379 && export LOCALHOST=localhost && export PYTHON=/opt/_internal/cpython-3.6.0/bin/python3
+
+cd /PaddleFL/python/paddle_fl/mpc/examples/lenet_with_mnist 
+cp ../logistic_with_mnist/process_data.py . && cp ../logistic_with_mnist/decrypt_save.py . 
+
+# change class_num = 10
+vim process_data.py
+
+bash run_standalone.sh train_lenet.py
+python3 decrypt_save.py decrypt_file
