b8c77f3686b52db5761ff8ebbe381f1998217a27
YueWang1996 <YueWang1996@users.noreply.github.com>
2021-05-26 12:03:07 +0200

    Create logistic_with_mnist.sh

---
+++ Glibc/test/logistic_with_mnist.sh
@@ -0,0 +1,9 @@
+# commands for https://github.com/PaddlePaddle/PaddleFL/blob/master/python/paddle_fl/mpc/examples/logistic_with_mnist/README.md
+
+# export PATH_TO_REDIS_BIN=/usr/local/bin/ && export REDIS_PORT=6379 && export LOCALHOST=localhost && export PYTHON=/opt/_internal/cpython-3.6.0/bin/python3
+
+cd /PaddleFL/python/paddle_fl/mpc/examples/logistic_with_mnist
+cp /PaddleFL/python/paddle_fl/mpc/examples/run_standalone.sh .
+python3 process_data.py
+bash run_standalone.sh train_fc_sigmoid.py
+python3 decrypt_save.py decrypt_file
