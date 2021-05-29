ce37c230fb653abcb887ea31d27d18e042b88d97
YueWang1996 <YueWang1996@users.noreply.github.com>
2021-05-29 17:36:42 +0200

    Update train.sh

--- Glibc/test/train.sh
+++ Glibc/test/train.sh
@@ -6,5 +6,6 @@ cd /PaddleFL/python/paddle_fl/mpc/examples/model_encryption/train \
 && python3 ../process_data.py \
 && cd ../train \
 && cp -r mpc_data/ ../ \
-&& bash ../run_standalone.sh encrypt_and_train_model.py \
+&& cp /PaddleFL/python/paddle_fl/mpc/examples/run_standalone.sh . \
+&& bash run_standalone.sh encrypt_and_train_model.py \
 && python3 decrypt_mpc_loss.py
