c516c2ff305631faa404cf554ca5656d3c907370
YueWang1996 <YueWang1996@users.noreply.github.com>
2021-05-29 18:03:34 +0200

    Update train.sh

--- Glibc/test/train.sh
+++ Glibc/test/train.sh
@@ -1,6 +1,6 @@
 # commands for https://github.com/PaddlePaddle/PaddleFL/tree/master/python/paddle_fl/mpc/examples/model_encryption/train
 
-export PATH_TO_REDIS_BIN=/usr/local/bin/ && export REDIS_PORT=6379 && export LOCALHOST=localhost && export PYTHON=/opt/_internal/cpython-3.6.0/bin/python3
+#export PATH_TO_REDIS_BIN=/usr/local/bin/ && export REDIS_PORT=6379 && export LOCALHOST=localhost && export PYTHON=/opt/_internal/cpython-3.6.0/bin/python3
 
 cd /PaddleFL/python/paddle_fl/mpc/examples/model_encryption/train \
 && python3 ../process_data.py \
