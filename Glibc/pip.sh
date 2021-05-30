1c573561a5baf9a6b0ba6690921700daa3e39958
YueWang1996 <YueWang1996@users.noreply.github.com>
2021-05-30 14:58:59 +0200

    Update pip.sh

--- Glibc/pip.sh
+++ Glibc/pip.sh
@@ -1,21 +1,30 @@
 # run this file inside the container built on Glibc image (see command on second line)
-# docker run -it --name scone-fl-pip -v $PWD:/demo --privileged --device /dev/isgx registry.scontain.com:5050/lequocdo/scone-paddle:paddle-fl-1.8.0
+# docker run -it --name scone-fl-pip -v $PWD:/demo --privileged --device registry.scontain.com:5050/lequocdo/scone-paddle:paddle-fl-1.0.1-paddlepaddle1.8.0
 
 # details of environment (after all compilation including comments):
 #   Ubuntu 20.04.2 LTS
+#   Python 3.6.9 |Anaconda, Inc.| (default, Jul 30 2019, 19:07:31)
+#       [GCC 7.3.0] on linux
 #   gcc (Ubuntu 9.3.0-17ubuntu1~20.04) 9.3.0
-#   g++ (Ubuntu 9.3.0-17ubuntu1~20.04) 9.3.0
-#   cmake version 3.16.0
-#   Python 3.7.7 
-#   pip 21.0.1 from /root/miniconda/lib/python3.7/site-packages/pip (python 3.7)
+#   g++ ?
+#   cmake version ?
+#   pip 21.1.1 from /root/miniconda/lib/python3.6/site-packages/pip (python 3.6)
 #   Redis server v=5.0.8
 
 apt-get update \
-    && apt-get install -y libgomp1 patchelf \
-    && pip3 install paddle_fl \
-    && pip3 uninstall paddlepaddle \
-    && pip3 uninstall paddlepaddle-gpu \
-    && pip3 install paddlepaddle==1.8.0
+    && apt-get install -y libgl1-mesa-dev ffmpeg libsm6 libxext6 wget make gcc  \
+    && wget --no-check-certificate https://paddlefl.bj.bcebos.com/redis-stable.tar \
+    && tar -xf redis-stable.tar && cd redis-stable &&  make && make install \
+    && cd /  \
+    && git clone https://github.com/PaddlePaddle/PaddleFL.git \
+    && export PATH_TO_REDIS_BIN=/usr/local/bin/ && export REDIS_PORT=6379 && export LOCALHOST=localhost && export PYTHON=/usr/bin/python
+
+# apt-get update \
+#     && apt-get install -y libgomp1 patchelf \
+#     && pip3 install paddle_fl \
+#     && pip3 uninstall paddlepaddle \
+#     && pip3 uninstall paddlepaddle-gpu \
+#     && pip3 install paddlepaddle==1.8.0
 
 
 # wget --no-check-certificate https://paddlefl.bj.bcebos.com/redis-stable.tar \
